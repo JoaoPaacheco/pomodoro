141c1eae8405ae1b2b26d23600a2d50180551c43
Joao Pedro Pacheco Rodrigues Almeida <joao_pra@hotmail.com>
2024-09-25 19:31:20 -0300

    Iniciando formulário de tempo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+import { Label } from './components/ui/label'
 import './index.css'
 
 export function App() {
   return (
-    <main className='h-screen flex items-center justify-center'>
-      <Button>Iniciar</Button>
-    </main>
+    <div className="flex h-screen items-center justify-center bg-[url('src/assets/forest-starry-sky-night.jpg')] bg-cover p-8">
+      <main className='p-4 bg-white/10 backdrop-blur rounded-lg w-full text-white'>
+        <div className='mb-4'>
+          <div>
+            <Label htmlFor='concentrationMinutes'>Tempo de concentração:</Label>
+            <Input type='number' name='concentrationMinutes'/>
+          </div>
+          <div>
+            <Label htmlFor='restMinutes'>Tempo de pausa:</Label>
+            <Input type='number' name='restMinutes'/>
+          </div>
+        </div>
+        <Button className='w-full font-bold'>Começar</Button>
+      </main>
+    </div>
   )
 }
