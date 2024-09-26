b013fe0ec5070a683bb5dc44ebc8efac08567b4e
Joao Pedro Pacheco Rodrigues Almeida <joao_pra@hotmail.com>
2024-09-26 18:58:33 -0300

    feat: drawer de configuração

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
+import { CustomDrawer } from './components/customDrawer'
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 import { Label } from './components/ui/label'
 import './index.css'
 
 export function App() {
+  const [focusTime, setFocusTime] = useState<number>(25)
+  const [restTime, setRestTime] = useState<number>(5)
+  const [pomodoroAmount, setPomodoroAmount] = useState<number>(3)
+  const [isActive, setIsActive] = useState<boolean>(false)
+  
   return (
-    <div className="flex h-screen items-center justify-center bg-[url('src/assets/forest-starry-sky-night.jpg')] bg-cover p-8">
+    // <div className="flex h-screen items-center justify-center bg-[url('src/assets/forest-starry-sky-night.jpg')] bg-cover p-8">
+    <div className="flex h-screen items-center justify-center bg-cover p-8 bg-gray-800">
       <main className='p-4 bg-white/10 backdrop-blur rounded-lg w-full text-white'>
         <div className='mb-4'>
           <div>
@@ -18,6 +26,7 @@ export function App() {
           </div>
         </div>
         <Button className='w-full font-bold'>Começar</Button>
+        <CustomDrawer focusTime={focusTime} restTime={restTime} setFocusTime={setFocusTime} setRestTime={setRestTime}/>
       </main>
     </div>
   )
---
+++ src/components/CustomDrawer.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react"
+import { Minus, Plus } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
+interface ICustomDrawer {
+    focusTime: number;
+    setFocusTime: React.Dispatch<React.SetStateAction<number>>;
+
+    restTime: number;
+    setRestTime: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export function CustomDrawer({focusTime, setFocusTime, restTime, setRestTime}: ICustomDrawer) {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button className="w-full mt-2">Configuração</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Configuração</DrawerTitle>
+            <DrawerDescription>Defina seus tempos de foco e descanso</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+            <div className="flex items-center justify-center space-x-2">
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => setFocusTime(prev => prev - 5)}
+                disabled={focusTime <= 5}
+              >
+                <Minus className="h-4 w-4" />
+              </Button>
+              <div className="flex-1 text-center">
+                <div className="text-7xl font-bold tracking-tighter">
+                  {focusTime}
+                </div>
+                <div className="text-[0.70rem] uppercase text-muted-foreground">
+                  Foco
+                </div>
+              </div>
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => setFocusTime(prev => prev + 5)}
+              >
+                <Plus className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+          <div className="p-4 pb-0">
+            <div className="flex items-center justify-center space-x-2">
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => setRestTime(prev => prev - 1)}
+                disabled={restTime <= 1}
+
+              >
+                <Minus className="h-4 w-4" />
+              </Button>
+              <div className="flex-1 text-center">
+                <div className="text-7xl font-bold tracking-tighter">
+                  {restTime}
+                </div>
+                <div className="text-[0.70rem] uppercase text-muted-foreground">
+                  Descanso
+                </div>
+              </div>
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => setRestTime(prev => prev + 1)}
+              >
+                <Plus className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button>Salvar</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
