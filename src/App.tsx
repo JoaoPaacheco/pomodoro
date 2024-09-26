8a9611014e2ba269705ef5179c49ae2035599185
Joao Pedro Pacheco Rodrigues Almeida <joao_pra@hotmail.com>
2024-09-26 19:26:22 -0300

    Preparação para tela inicial

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,19 @@ export function App() {
   const [restTime, setRestTime] = useState<number>(5)
   const [pomodoroAmount, setPomodoroAmount] = useState<number>(3)
   const [isActive, setIsActive] = useState<boolean>(false)
+
+  const beginPomodoro = () => {
+    setIsActive(prev => !prev)
+  }
+
+  console.log(isActive)
   
   return (
-    // <div className="flex h-screen items-center justify-center bg-[url('src/assets/forest-starry-sky-night.jpg')] bg-cover p-8">
-    <div className="flex h-screen items-center justify-center bg-cover p-8 bg-gray-800">
-      <main className='p-4 bg-white/10 backdrop-blur rounded-lg w-full text-white'>
-        <div className='mb-4'>
-          <div>
-            <Label htmlFor='concentrationMinutes'>Tempo de concentração:</Label>
-            <Input type='number' name='concentrationMinutes'/>
-          </div>
-          <div>
-            <Label htmlFor='restMinutes'>Tempo de pausa:</Label>
-            <Input type='number' name='restMinutes'/>
-          </div>
-        </div>
-        <Button className='w-full font-bold'>Começar</Button>
+    <div className="flex h-screen items-center justify-center bg-[url('src/assets/forest-starry-sky-night.jpg')] bg-cover p-8">
+    {/* <div className="flex h-screen items-center justify-center bg-cover p-8 bg-gray-800 dark"> */}
+      <main className='p-4 bg-white/10 backdrop-blur rounded-lg w-full'>
+        <h1 className='text-center text-3xl font-bold'>Título</h1>
+        <Button className='w-full font-bold' onClick={beginPomodoro}>Começar</Button>
         <CustomDrawer focusTime={focusTime} restTime={restTime} setFocusTime={setFocusTime} setRestTime={setRestTime}/>
       </main>
     </div>
